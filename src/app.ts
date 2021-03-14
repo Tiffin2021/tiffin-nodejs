0b48bdffaa9b29c24f6cb82f7566690a0bdf5c0a
mattsershaw <buddylfs@gmail.com>
2021-03-14 14:27:03 +0900

    controller(getAllのみ)

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { Client } from 'pg';
 import { Request, Response, Router } from 'express';
 import { ShopAccountRepository } from './repository/ShopAccountRepository';
 import { ShopAccountService } from './service/ShopAccountService';
+import { ShopAccountController } from './controller/ShopAccountController';
 
 //定義
 const app = express();
@@ -38,12 +39,6 @@ connection
 
 const repository = new ShopAccountRepository(connection);
 const service = new ShopAccountService(repository);
+const controller = new ShopAccountController(service);
 
-app.get('/api/shop_accounts/', async (req: Request, res: Response) => {
-  const shopAccounts = await service.getAll().catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-    return;
-  });
-  res.json(shopAccounts);
-});
+app.use('/api/', controller.router);
---
+++ src/controller/ShopAccountController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, Router } from 'express';
+import { IShopAccountService } from '../service/interfaces/IShopAccountService';
+
+export class ShopAccountController {
+  public router: Router;
+  private service: IShopAccountService;
+
+  constructor(service: IShopAccountService) {
+    this.router = Router();
+    this.service = service;
+
+    this.router.get('/shop_accounts', async (req: Request, res: Response) => {
+      const shopAccounts = await this.service.getAll().catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        return;
+      });
+      res.json(shopAccounts);
+    });
+  }
+}
