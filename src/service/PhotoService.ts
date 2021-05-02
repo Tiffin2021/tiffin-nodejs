f685de07dc4fd1428797741724d416a9338ab75c
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-05-02 17:14:12 +0900

    拡張子をアップロードした画像から取得できるように修正(変数名のセンスがないから誰かリネームして)

--- src/service/PhotoService.ts
+++ src/service/PhotoService.ts
@@ -114,18 +114,21 @@ export class PhotoService implements IPhotoService {
   async create(shopAccountId: number, photo: Photo): Promise<Result<number>> {
     // Controllerに返却するための結果オブジェクトを生成
     const result: Result<number> = {};
-    //ここで画像ファイルのパスを決定しておく必要がある現在の値は(仮)
-    photo.pass = dataImgPath + 'test.jpg';
+    // 拡張子を取得
+    const extension = photo.img
+      .toString()
+      .slice(photo.img.indexOf('/') + 1, photo.img.indexOf(';'));
     // 余分な部分を削る
     const imgDataString = photo.img.replace(/^data:\w+\/\w+;base64,/, '');
     // base64 デコード
     const img = Buffer.from(imgDataString, 'base64');
     // サーバーに保存する
-    fs.writeFile(uploadImgPath + 'test.jpg', img, (err) => {
+    fs.writeFile(`${uploadImgPath}test.${extension}`, img, (err) => {
       if (err) throw err;
       console.log('正常に書き込みが完了しました');
     });
-
+    //ここで画像ファイルのパスを決定しておく必要がある現在の値は(仮)
+    photo.pass = `${dataImgPath}test.${extension}`;
     // 店舗情報を1件取得する(現在の使用ではアカウントに対して店舗情報が1件取得でいるはずなので、、、)
     const shopInfoResult = await this.shopInfoRepository.getByID(shopAccountId);
 
