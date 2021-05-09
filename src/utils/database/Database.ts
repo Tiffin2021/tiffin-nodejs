23e3b827c3f3c0b02abe7ab5f4901aa292cae941
hajime takeuchi <shadowthe8156@gmail.com>
2021-05-09 17:17:41 +0900

    SSL設定変更

--- src/utils/database/Database.ts
+++ src/utils/database/Database.ts
@@ -19,8 +19,12 @@ type DBConfig_Local = {
 
 type DBConfig_Production = {
   connectionString: string;
-  ssl: boolean;
+  ssl: {
+    sslmode: 'require';
+    rejectUnauthorized: false;
+  };
 };
+
 export class Database {
   private connection: Client;
 
@@ -67,7 +71,6 @@ export class Database {
     console.log(process.env.DATABASE_URL!);
     return {
       connectionString: process.env.DATABASE_URL!,
-      ssl: true,
     } as DBConfig_Production;
   }
 
